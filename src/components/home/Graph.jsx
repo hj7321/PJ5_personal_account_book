c82733adf44dae014047acdc094bfcfef9caeb3b
hj7321 <oboki2001@naver.com>
2024-05-24 18:39:03 +0900

    Graph 컴포넌트에 항목별 총 비용 및 비율 구현 완료

--- src/components/home/Graph.jsx
+++ src/components/home/Graph.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { MonthContext, ExpenseContext } from "../../context/SharedContext";
-import { StSection } from "../style/CalendarStyle";
-import { StH3 } from "../style/GraphStyle";
+import { StH3, StSection } from "../style/GraphStyle";
+import { StDiv } from "./../style/GraphStyle";
 
 const Graph = () => {
   const { expense } = useContext(ExpenseContext);
@@ -11,20 +11,33 @@ const Graph = () => {
     (obj) => obj.date.split("-")[1] == month
   );
 
+  const itemObj = filteredExpense.reduce((accObj, curObj) => {
+    if (accObj[curObj.item]) accObj[curObj.item] += curObj.amount;
+    else accObj[curObj.item] = curObj.amount;
+    return accObj;
+  }, {});
+
+  const changeArrToItemObj = Object.entries(itemObj);
+
+  const totalAmount = filteredExpense.reduce((amount, obj) => {
+    return amount + obj.amount;
+  }, 0);
+
   return (
     <StSection>
       <StH3>
         {month}월 총 지출: 💸
-        {filteredExpense
-          ? filteredExpense
-              .reduce((amount, obj) => {
-                return amount + obj.amount;
-              }, 0)
-              .toLocaleString()
-          : 0}
-        원
+        {filteredExpense ? totalAmount.toLocaleString() : 0}원
       </StH3>
       {/* 그래프 나타내기 */}
+      <StDiv>
+        {changeArrToItemObj.map((cur) => (
+          <p key={cur[0]}>
+            {cur[0]}: {cur[1].toLocaleString()}원 (
+            {Math.round((cur[1] / totalAmount) * 100 * 100) / 100}%)
+          </p>
+        ))}
+      </StDiv>
     </StSection>
   );
 };
