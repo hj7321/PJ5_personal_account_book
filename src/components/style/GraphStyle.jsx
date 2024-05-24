c9ea346c21fc35b0727d7e98ba1bf775182e6118
hj7321 <oboki2001@naver.com>
2024-05-24 18:39:03 +0900

    Graph 컴포넌트에 항목별 총 비용 및 비율 구현 완료

--- src/components/style/GraphStyle.jsx
+++ src/components/style/GraphStyle.jsx
@@ -4,3 +4,16 @@ export const StH3 = styled.h3`
   font-size: 25px;
   margin: 20px;
 `;
+
+export const StSection = styled.section`
+  flex-direction: column;
+  text-align: center;
+`;
+
+export const StDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+  margin: 20px;
+`;
